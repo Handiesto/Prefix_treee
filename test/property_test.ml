c31d5fc05b1aefaf0ca89bf0a9d3bd464380da4a
Handiesto <klim.plyasov@gmail.com>
2023-03-17 05:42:06 +0300

    property

--- test/property_test.ml
+++ test/property_test.ml
@@ -2,48 +2,43 @@ open! OUnit2
 open! QCheck
 open! Prefix_tree
 
-
-let insertToTrie  =
+let insertToTrie =
   QCheck.Test.make ~count:1000 ~name:"insert trie"
-    QCheck.(pair string (small_nat))
+    QCheck.(pair string small_nat)
     (fun (key, value) ->
       let tree0 = Prefix_tree.empty in
-            let tree1 = Prefix_tree.add key value tree0 in
-            match Prefix_tree.find key tree1 with
-            | Some v -> v = value
-            | None -> false)
+      let tree1 = Prefix_tree.add key value tree0 in
+      match Prefix_tree.find key tree1 with
+      | Some v -> v = value
+      | None -> false )
 
 let removeFromTrie =
   QCheck.Test.make ~count:1000 ~name:"remove from trie"
-    QCheck.(pair string (small_nat))
+    QCheck.(pair string small_nat)
     (fun (key, value) ->
-       let tree0 = Prefix_tree.empty in
-       let tree1 = Prefix_tree.add key value tree0 in
-       let tree2 = Prefix_tree.remove key tree1 in
-       match Prefix_tree.find key tree2 with
-       | Some _ -> false
-       | None -> true
-            )
+      let tree0 = Prefix_tree.empty in
+      let tree1 = Prefix_tree.add key value tree0 in
+      let tree2 = Prefix_tree.remove key tree1 in
+      match Prefix_tree.find key tree2 with Some _ -> false | None -> true )
 
-
-let prop_prefix_tree_map  =
-    QCheck.Test.make ~count:1000 ~name:"insert trie"
-    QCheck.(pair string (small_nat))
+let prop_prefix_tree_map =
+  QCheck.Test.make ~count:1000 ~name:"insert trie"
+    QCheck.(pair string small_nat)
     (fun (key, value) ->
       let tree0 = Prefix_tree.empty in
       let tree1 = Prefix_tree.add key value tree0 in
       let tree2 = Prefix_tree.map (fun x -> x * 2) tree1 in
       match Prefix_tree.find key tree2 with
       | Some v -> v = value * 2
-      | None -> false)
+      | None -> false )
 
 let prop_prefix_tree_merge =
   QCheck.Test.make ~count:1000 ~name:"merge trie"
-    QCheck.(triple (pair string (small_nat))
-                        (pair (small_nat) (small_nat))
-                        (pair (small_nat) (small_nat))
-                        )
-    (fun ((key,value1),(value2,value3),(value4,value5)) ->
+    QCheck.(
+      triple (pair string small_nat)
+        (pair small_nat small_nat)
+        (pair small_nat small_nat) )
+    (fun ((key, value1), (value2, value3), (value4, value5)) ->
       let key_a = key ^ "a" in
       let key_b = key ^ "b" in
       let key_c = key ^ "c" in
@@ -58,68 +53,77 @@ let prop_prefix_tree_merge =
       let node4 = Prefix_tree.add key_b value5 Prefix_tree.empty in
       let node5 = Prefix_tree.add key_d value4 node4 in
       let merged = Prefix_tree.merge node3 node5 in
-      let equals1 = expected_a = Option.value ~default:(-1) (Prefix_tree.find key_a merged) in
-      let equals2 = expected_b = Option.value ~default:(-1) (Prefix_tree.find key_b merged) in
-      let equals3 = expected_c = Option.value ~default:(-1) (Prefix_tree.find key_c merged) in
-      let equals4 = expected_d = Option.value ~default:(-1) (Prefix_tree.find key_d merged) in
-      equals1 && equals2 && equals3 && equals4)
-
-
+      let equals1 =
+        expected_a
+        = Option.value ~default:(-1) (Prefix_tree.find key_a merged)
+      in
+      let equals2 =
+        expected_b
+        = Option.value ~default:(-1) (Prefix_tree.find key_b merged)
+      in
+      let equals3 =
+        expected_c
+        = Option.value ~default:(-1) (Prefix_tree.find key_c merged)
+      in
+      let equals4 =
+        expected_d
+        = Option.value ~default:(-1) (Prefix_tree.find key_d merged)
+      in
+      equals1 && equals2 && equals3 && equals4 )
 
 let associative =
-    QCheck.Test.make ~count:1000 ~name:"associative"
-      QCheck.(quad (pair string (small_nat))
-                      (pair string (small_nat))
-                      (pair string (small_nat))
-                      (pair string (small_nat)))
-        (fun ((key1,value1),(key2,value2),(key3,value3),(key4,value4)) ->
-        let node1 = Prefix_tree.add key1 value1 Prefix_tree.empty in
-        let node2 = Prefix_tree.add key2 (value2) node1 in
-        let t1 = Prefix_tree.add key3 (value3) node2 in
-        let node4 = Prefix_tree.add key2 (value2) Prefix_tree.empty in
-        let t2 = Prefix_tree.add key4 (value4) node4 in
-        let node6 = Prefix_tree.add key3 (value3) Prefix_tree.empty in
-        let t3 = Prefix_tree.add key4 (value4) node6 in
-        let lhs = Prefix_tree.merge (Prefix_tree.merge t1 t2) t3 in
-        let rhs = Prefix_tree.merge t1 (Prefix_tree.merge t2 t3) in
-        Prefix_tree.isSame lhs rhs)
+  QCheck.Test.make ~count:1000 ~name:"associative"
+    QCheck.(
+      quad (pair string small_nat) (pair string small_nat)
+        (pair string small_nat) (pair string small_nat) )
+    (fun ((key1, value1), (key2, value2), (key3, value3), (key4, value4)) ->
+      let node1 = Prefix_tree.add key1 value1 Prefix_tree.empty in
+      let node2 = Prefix_tree.add key2 value2 node1 in
+      let t1 = Prefix_tree.add key3 value3 node2 in
+      let node4 = Prefix_tree.add key2 value2 Prefix_tree.empty in
+      let t2 = Prefix_tree.add key4 value4 node4 in
+      let node6 = Prefix_tree.add key3 value3 Prefix_tree.empty in
+      let t3 = Prefix_tree.add key4 value4 node6 in
+      let lhs = Prefix_tree.merge (Prefix_tree.merge t1 t2) t3 in
+      let rhs = Prefix_tree.merge t1 (Prefix_tree.merge t2 t3) in
+      Prefix_tree.isSame lhs rhs )
 
 let identity =
-   QCheck.Test.make ~count:1000 ~name:"associative"
-    QCheck.(triple (pair string (small_nat))
-                    (pair string (small_nat))
-                    (pair string (small_nat))
-                    )
-      (fun ((key1,value1),(key2,value2),(key3,value3)) ->
+  QCheck.Test.make ~count:1000 ~name:"associative"
+    QCheck.(
+      triple (pair string small_nat) (pair string small_nat)
+        (pair string small_nat) )
+    (fun ((key1, value1), (key2, value2), (key3, value3)) ->
       let node1 = Prefix_tree.add key1 value1 Prefix_tree.empty in
-      let node2 = Prefix_tree.add key2 (value2) node1 in
-      let t = Prefix_tree.add key3 (value3) node2 in
+      let node2 = Prefix_tree.add key2 value2 node1 in
+      let t = Prefix_tree.add key3 value3 node2 in
       let lhs = Prefix_tree.merge t Prefix_tree.empty in
       let rhs = t in
-      Prefix_tree.isSame lhs rhs)
+      Prefix_tree.isSame lhs rhs )
 
- let commutative =
-    QCheck.Test.make ~count:1000 ~name:"commutative"
-      QCheck.(quad (pair string (small_nat))
-                      (pair string (small_nat))
-                      (pair string (small_nat))
-                      (pair string (small_nat)))
-       (fun ((key1,value1),(key2,value2),(key3,value3),(key4,value4)) ->
-        let node1 = Prefix_tree.add key1 value1 Prefix_tree.empty in
-        let node2 = Prefix_tree.add key2 (value2) node1 in
-        let t1 = Prefix_tree.add key3 (value3) node2 in
-        let node4 = Prefix_tree.add key2 (value2) Prefix_tree.empty in
-        let t2 = Prefix_tree.add key4 (value4) node4 in
-        let lhs = Prefix_tree.merge t1 t2 in
-        let rhs = Prefix_tree.merge t2 t1 in
-        Prefix_tree.isSame lhs rhs)
-
-
+let commutative =
+  QCheck.Test.make ~count:1000 ~name:"commutative"
+    QCheck.(
+      quad (pair string small_nat) (pair string small_nat)
+        (pair string small_nat) (pair string small_nat) )
+    (fun ((key1, value1), (key2, value2), (key3, value3), (key4, value4)) ->
+      let node1 = Prefix_tree.add key1 value1 Prefix_tree.empty in
+      let node2 = Prefix_tree.add key2 value2 node1 in
+      let t1 = Prefix_tree.add key3 value3 node2 in
+      let node4 = Prefix_tree.add key2 value2 Prefix_tree.empty in
+      let t2 = Prefix_tree.add key4 value4 node4 in
+      let lhs = Prefix_tree.merge t1 t2 in
+      let rhs = Prefix_tree.merge t2 t1 in
+      Prefix_tree.isSame lhs rhs )
 
 let _ =
   let open OUnit in
   run_test_tt_main
     ( "tests"
     >::: List.map QCheck_ounit.to_ounit_test
-           [insertToTrie; removeFromTrie; prop_prefix_tree_map; associative; identity; prop_prefix_tree_merge])
-
+           [ insertToTrie
+           ; removeFromTrie
+           ; prop_prefix_tree_map
+           ; associative
+           ; identity
+           ; prop_prefix_tree_merge ] )
--- test/test_unit.ml
+++ test/test_unit.ml
@@ -6,12 +6,12 @@ let test_add _ =
   let trie2 = add "hello" 1 trie1 in
   let trie3 = add "world" 2 trie2 in
   let trie4 = add "hello" 3 trie3 in
-  assert_equal (find "hello" trie1) None;
-  assert_equal (find "hello" trie2) (Some 1);
-  assert_equal (find "world" trie2) None;
-  assert_equal (find "hello" trie3) (Some 1);
-  assert_equal (find "world" trie3) (Some 2);
-  assert_equal (find "hello" trie4) (Some 3);
+  assert_equal (find "hello" trie1) None ;
+  assert_equal (find "hello" trie2) (Some 1) ;
+  assert_equal (find "world" trie2) None ;
+  assert_equal (find "hello" trie3) (Some 1) ;
+  assert_equal (find "world" trie3) (Some 2) ;
+  assert_equal (find "hello" trie4) (Some 3) ;
   assert_equal (find "world" trie4) (Some 2)
 
 let test_remove _ =
@@ -19,78 +19,73 @@ let test_remove _ =
   let trie2 = add "hello" 1 trie1 in
   let trie3 = add "world" 2 trie2 in
   let trie4 = remove "hello" trie3 in
-  assert_equal (find "hello" trie1) None;
-  assert_equal (find "hello" trie2) (Some 1);
-  assert_equal (find "world" trie2) None;
-  assert_equal (find "hello" trie3) (Some 1);
-  assert_equal (find "world" trie3) (Some 2);
-  assert_equal (find "hello" trie4) None;
+  assert_equal (find "hello" trie1) None ;
+  assert_equal (find "hello" trie2) (Some 1) ;
+  assert_equal (find "world" trie2) None ;
+  assert_equal (find "hello" trie3) (Some 1) ;
+  assert_equal (find "world" trie3) (Some 2) ;
+  assert_equal (find "hello" trie4) None ;
   assert_equal (find "world" trie4) (Some 2)
-
 
 let test_find _ =
   let trie1 = empty in
   let trie2 = add "hello" 1 trie1 in
   let trie3 = add "world" 2 trie2 in
   let trie4 = add "hello" 3 trie3 in
-  assert_equal (find "hello" trie1) None;
-  assert_equal (find "hello" trie2) (Some 1);
-  assert_equal (find "world" trie2) None;
-  assert_equal (find "hello" trie3) (Some 1);
-  assert_equal (find "world" trie3) (Some 2);
-  assert_equal (find "hello" trie4) (Some 3);
+  assert_equal (find "hello" trie1) None ;
+  assert_equal (find "hello" trie2) (Some 1) ;
+  assert_equal (find "world" trie2) None ;
+  assert_equal (find "hello" trie3) (Some 1) ;
+  assert_equal (find "world" trie3) (Some 2) ;
+  assert_equal (find "hello" trie4) (Some 3) ;
   assert_equal (find "world" trie4) (Some 2)
-
-
 
 (* Test for empty trie *)
 let test_filter _ =
   let t = empty in
   let filtered_t = filter (fun _ -> true) t in
-  assert (filtered_t = Empty);
-
+  assert (filtered_t = Empty) ;
   let t = add "a" 1 empty in
   let filtered_t = filter (fun _ -> true) t in
-  assert (isSame t filtered_t);
-
+  assert (isSame t filtered_t) ;
   let t = add "a" 1 (add "b" 2 (add "c" 3 empty)) in
   let filtered_t = filter (fun _ -> true) t in
-  assert (isSame t filtered_t);
+  assert (isSame t filtered_t) ;
   let t = add "a" 1 (add "b" 2 (add "c" 3 empty)) in
   let filtered_t = filter (fun _ -> false) t in
-  assert (filtered_t = Empty);
+  assert (filtered_t = Empty) ;
   let t = add "a" 1 (add "b" 2 (add "c" 3 empty)) in
   let filtered_t = filter (fun _ -> true) t in
   assert (isSame t filtered_t)
 
-
 let test_map _ =
   let t1 = empty |> add "hello" 1 |> add "world" 2 in
   let t2 = map (fun v -> v * 2) t1 in
-  assert_equal (Some 1) (find "hello" t1);
-  assert_equal (Some 2) (find "world" t1);
-  assert_equal (Some 2) (find "hello" t2);
+  assert_equal (Some 1) (find "hello" t1) ;
+  assert_equal (Some 2) (find "world" t1) ;
+  assert_equal (Some 2) (find "hello" t2) ;
   assert_equal (Some 4) (find "world" t2)
 
 let test_merge _ =
   let t1 = add "abc" 1 (add "ab" 2 (add "a" 3 empty)) in
   let t2 = add "abc" 4 (add "a" 5 (add "de" 6 empty)) in
   let t3 = merge t1 t2 in
-  assert_equal (Some 3) (find "a" t3);
-  assert_equal (Some 2) (find "ab" t3);
-  assert_equal (Some 1) (find "abc" t3);
-  assert_equal (Some 6) (find "de" t3);
-  assert_equal None (find "xyz" t3);
+  assert_equal (Some 3) (find "a" t3) ;
+  assert_equal (Some 2) (find "ab" t3) ;
+  assert_equal (Some 1) (find "abc" t3) ;
+  assert_equal (Some 6) (find "de" t3) ;
+  assert_equal None (find "xyz" t3) ;
   assert_equal None (find "abcd" t3)
 
 let test_fold_left _ =
   let t =
-    Node (Some "hello",
-      CharMap.of_seq @@ List.to_seq
-        [ ('w', Node (None, CharMap.of_seq @@ List.to_seq []))
-        ; ('o', Node (Some "world", CharMap.of_seq @@ List.to_seq []))
-        ]
-    )
+    Node
+      ( Some "hello"
+      , CharMap.of_seq
+        @@ List.to_seq
+             [ ('w', Node (None, CharMap.of_seq @@ List.to_seq []))
+             ; ('o', Node (Some "world", CharMap.of_seq @@ List.to_seq []))
+             ] )
   in
   let expected = "helloworld" in
   let actual = fold_left (fun acc x -> acc ^ x) "" t in
@@ -98,31 +93,27 @@ let test_fold_left _ =
 
 let test_fold_right _ =
   let t =
-    Node (Some "hello",
-      CharMap.of_seq @@ List.to_seq
-        [ ('w', Node (None, CharMap.of_seq @@ List.to_seq []))
-        ; ('o', Node (Some "world", CharMap.of_seq @@ List.to_seq []))
-        ]
-    )
+    Node
+      ( Some "hello"
+      , CharMap.of_seq
+        @@ List.to_seq
+             [ ('w', Node (None, CharMap.of_seq @@ List.to_seq []))
+             ; ('o', Node (Some "world", CharMap.of_seq @@ List.to_seq []))
+             ] )
   in
   let expected = "helloworld" in
   let actual = fold_right (fun x acc -> x ^ acc) t "" in
   assert (actual = expected)
 
-
 let suite =
-  "test suite: " >::: [
-    "test_add: " >:: test_add;
-    "test_remove: " >:: test_remove;
-    "test_find: " >:: test_find;
-    "test_filter" >:: test_filter;
-    "test_map"    >:: test_map;
-    "test_merge"  >:: test_merge;
-    "test_fold_left" >:: test_fold_left;
-    "test_fold_right" >:: test_fold_right
-  ]
+  "test suite: "
+  >::: [ "test_add: " >:: test_add
+       ; "test_remove: " >:: test_remove
+       ; "test_find: " >:: test_find
+       ; "test_filter" >:: test_filter
+       ; "test_map" >:: test_map
+       ; "test_merge" >:: test_merge
+       ; "test_fold_left" >:: test_fold_left
+       ; "test_fold_right" >:: test_fold_right ]
 
-let () =
-  run_test_tt_main suite;
-
-
+let () = run_test_tt_main suite
